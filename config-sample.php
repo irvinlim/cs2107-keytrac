502b997fef53da64007c2832d85a7fb74582766e
Irvin Lim <irvinlimweiquan@gmail.com>
2016-11-07 06:22:26 +0800

    Update config sample

--- config-sample.php
+++ config-sample.php
@@ -1,3 +1,12 @@
 <?php
+global $config;
 
 $config['keytrac_auth_token'] = '<AUTH_TOKEN>';
+
+$config['mysql_user'] = '<USERNAME>';
+$config['mysql_pass'] = '<PASSWORD>';
+$config['mysql_host'] = '<HOSTNAME>';
+$config['mysql_db'] = '<DATABASE>';
+
+// Enter a random string that will be used to salt the hashes.
+$config['random_salt'] = '';
